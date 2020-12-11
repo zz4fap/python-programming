72bf92f61fe73663e5ef109c102d4ca1e74418ac
Felipe Augusto Pereira de Figueiredo <felipe.figueiredo@inatel.br>
2020-12-11 16:03:26 -0300

    new scripts

--- provas/Game/Imagem/Processamento.py
+++
@@ -1,8 +0,0 @@
-def abrir():
-    return 'Abrindo imagem'
-
-def trocar():
-    return 'Trocando imagem'
-
-def fechar():
-    return 'Fechando imagem'
--- provas/Game/Som/Processamento.py
+++
@@ -1,8 +0,0 @@
-def carregar():
-    return 'Carregando som'
-
-def tocar():
-    return 'Tocando som'
-
-def pausar():
-    return 'Pausando som'
